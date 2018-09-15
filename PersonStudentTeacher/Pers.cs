e05bb4c768f5b2f13baea5d1845944e21e6dad23
unknown <vova111628@gmail.com>
2018-09-15 19:04:48 +0300

    Added

---
+++ PersonStudentTeacher/Pers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonStudentTeacher
+{
+    class Pers
+    {
+        private string name { get; set; }
+        private string addres { get; set; }
+        private int age { get; set; }
+    }
+}
