0e41d1d00fbd259a1e9134e5aa6ab93dd30d6e41
AndryushkaYO <andri.yovbak@gmail.com>
2018-09-19 20:30:08 +0300

    Created folders for Unit Tests

---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PersonStudentTeacher;
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
