9c9b430cf47447251c7431c8b19f93f5f6249203
AndryushkaYO <andri.yovbak@gmail.com>
2018-09-19 20:31:33 +0300

    Wrote tasks for Unit Tests

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,6 +1,26 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PersonStudentTeacher;
+///Створити
+/*
+ 1. Тести для класу Person
+    -конструктор
+    -Equals
+    -GetHashCode
+    -ToString
+ 2. Тести для класу Student
+    -конструктор
+    -Equals
+    -GetHashCode
+    -OutPut
+ 3. Тести для класу Teacher
+    -конструктор
+    -Equals
+    -GetHashCode
+    -OutPut
+ 4. SetStudents, ReadPersons, CloneList
+ */
+
 namespace UnitTest
 {
     [TestClass]
