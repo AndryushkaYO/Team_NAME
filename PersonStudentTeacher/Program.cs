d46bdf7e5314ba6b8fbdc3531bab4be3154c8558
Kuhivchak Volodya <vova111628@gmail.com>
2018-09-25 17:27:11 +0300

    Tabulation fixed

--- PersonStudentTeacher/Program.cs
+++ PersonStudentTeacher/Program.cs
@@ -8,16 +8,17 @@ namespace PersonStudentTeacher
 {
     class Program
     {
-       /// <summary>
-       /// Outputs the exception caught
-       /// </summary>
-       /// <param name="exception">Thrown exception</param>
-       /// <param name="message">Exception's message</param>
+        /// <summary>
+        /// Outputs the exception caught
+        /// </summary>
+        /// <param name="exception">Thrown exception</param>
+        /// <param name="message">Exception's message</param>
         public static void WriteExceptionMessage(Exception exception, string message)
         {
             Console.WriteLine($"Type: {message}\n\nMessage: {exception.Message}\n\nStackTrace: {exception.StackTrace}\n\nDate: {DateTime.Now.ToString()}");
             Console.WriteLine("\n\t-----------------------------------------------------------------------------\n");
         }
+		
         /// <summary>
         /// Main function
         /// </summary>
