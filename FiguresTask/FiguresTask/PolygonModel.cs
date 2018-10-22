71a8c2c281c971962bd6246bf2632843dfd4237c
AndryushkaYO <andri.yovbak@gmail.com>
2018-10-22 14:56:45 +0300

    Added packages and some changes

---
+++ FiguresTask/FiguresTask/PolygonModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Figures.DataAcces
+{
+    class PolygonModel
+    {
+        public List<Point> Points = new List<Point>();
+        public Color Color { get; set; }
+        public double Stroke { get; set; }
+
+        public PolygonModel() { }
+
+        public PolygonModel(List<Point> points, Color color, double stroke)
+        {
+            Points = points;
+            Color = color;
+            Stroke = stroke;
+        }
+    }
+}
