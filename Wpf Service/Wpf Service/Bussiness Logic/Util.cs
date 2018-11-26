bbdfc6a3032b1f89f445ed15c2f3b06d60943ee5
bogdanpetrovsky <bpetrovsky977@gmail.com>
2018-11-27 00:26:24 +0200

    Ended Data access and created business logic of the project

---
+++ Wpf Service/Wpf Service/Bussiness Logic/Util.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Wpf_Service.Bussiness_Logic
+{
+    public static class Util
+    {
+
+        public static void Error(string caption, string message)
+        {
+            MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+
+        public static void Info(string caption, string message)
+        {
+            MessageBox.Show(message, caption, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+}
---
+++ Wpf Service/Wpf Service/Models/StoreModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace Wpf_Service.Models
+{
+    class StoreModel
+    {
+        [Serializable]
+        public class ShopData
+        {
+
+            public string Name { get; set; }
+
+            public AddressModel AddressModel { get; set; }
+
+
+            public ShopData()
+            {
+                AddressModel = new AddressModel();
+            }
+
+
+            public ShopData(string name, AddressModel shopAddressModel)
+            {
+                Name = name.Trim();
+                AddressModel = shopAddressModel;
+            }
+
+            public ShopData(XmlNode source)
+            {
+
+                var attributes = source.Attributes;
+
+                Name = attributes["Name"].Value;
+                var AddressModelElement = source.SelectSingleNode("AddressModel");
+                AddressModel = new AddressModel(AddressModelElement.Attributes);
+            }
+
+            public XElement ToXml()
+            {
+                return new XElement(
+                    "ShopData",
+                    new XAttribute("Name", Name),
+                    AddressModel.ToXml());
+            }
+        }
+    }
+}
